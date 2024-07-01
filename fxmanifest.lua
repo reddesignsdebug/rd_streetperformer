cf4bbf3ae514670af2ef0668e2c6fdd3114f6043
reddesigns <123305087+reddesignsdebug@users.noreply.github.com>
2024-07-01 11:48:26 +0200

    reworked manifest and server.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
 fx_version 'cerulean'
-games 'gta5'
+game 'gta5'
 
 
 author 'RedDesigns'
--- server/server.lua
+++ server/server.lua
@@ -1,21 +1,18 @@
-
 -- Table to keep track of active performances
 local activePerformances = {}
 
-
 -- Event to start a performance
 RegisterServerEvent('streetPerformer:startPerformance')
-AddEventHandler(streetPerformer:startPerformance, function(coords)
-    local PlayerId = source
-    if activePerformances[PlayerId] then
-        TriggerClientEvent(streetPerformer:notify, playerId, 'You are already performing!')
+AddEventHandler('streetPerformer:startPerformance', function(coords)
+    local playerId = source
+    if activePerformances[playerId] then
+        TriggerClientEvent('streetPerformer:notify', playerId, 'You are already performing!')
         return
     end
 
     activePerformances[playerId] = coords
     TriggerClientEvent('streetPerformer:startPerformance', -1, playerId, coords)
 end)
-
 
 -- Event to stop a performance
 RegisterServerEvent('streetPerformer:stopPerformance')
@@ -40,4 +37,3 @@ AddEventHandler('streetPerformer:earnTips', function()
         -- Add logic to give the player the tip amount, e.g., using your server's money system
     end
 end)
-
